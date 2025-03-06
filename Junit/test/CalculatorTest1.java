eff1f9a463960cf684c47967aa4edf74e9bb9160
Bhagyakamath <120483354+Bhagyakamath@users.noreply.github.com>
2025-03-06 12:45:37 +0530

    Update CalculatorTest1.java

--- Junit/test/CalculatorTest1.java
+++ Junit/test/CalculatorTest1.java
@@ -1,9 +1,20 @@
 package com.test;
 import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
+import org.junit.platform.suite.api.IncludeClassNamePatterns;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
 
 @Timeout(value=5, unit=TimeUnit.SECONDS)
+@Suite
+@DisplayName("Test suite sample example")
+@SelectPackages("com.test")
+@IncludeClassNamePatterns(".*Test")
+@SelectClasses({CalculatorTest1.class, CalculatorTest.class})
 public class CalculatorTest1 {
 	Calculator obj;
 	@Test
@@ -20,3 +31,4 @@ public class CalculatorTest1 {
 	
 
 }
+
